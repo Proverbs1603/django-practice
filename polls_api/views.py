2892f5f80e1726b85b2fc906ea7db6768a6b5745
Proverbs1603 <110598358+Proverbs1603@users.noreply.github.com>
2024-10-11 18:21:21 +0900

    class기반으로 CRUD 재구현

--- polls_api/views.py
+++ polls_api/views.py
@@ -4,15 +4,15 @@ from polls_api.serializers import QuestionSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView 
 
-@api_view(['GET', 'POST'])
-def question_list(request):
-    if request.method == 'GET':
+class QuestionList(APIView):
+    def get(self, request):
         questions = Question.objects.all()
         serializer  = QuestionSerializer(questions, many = True)
-        return Response(serializer.data)
-    
-    if request.method == 'POST':
+        return Response(serializer.data)    
+
+    def post(self, request):
         serializer = QuestionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -20,15 +20,15 @@ def question_list(request):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def question_detail(request, id):
-    question = get_object_or_404(Question, pk=id)
-    
-    if request.method == 'GET':
+
+class QuestionDetail(APIView):
+    def get(self, request, id):
+        question = get_object_or_404(Question, pk=id)
         serializer = QuestionSerializer(question)
         return Response(serializer.data)
-
-    if request.method == 'PUT':
+        
+    def put(self, request, id):
+        question = get_object_or_404(Question, pk=id)
         serializer = QuestionSerializer(question, data = request.data)
         if serializer.is_valid():
             serializer.save()
@@ -36,7 +36,10 @@ def question_detail(request, id):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-    if request.method == 'DELETE':
+    def delete(self, request, id):
+        question = get_object_or_404(Question, pk=id)
         question.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+        
+
+
