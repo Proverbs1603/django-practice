5d302228c55bc206a8161f5619e65f3259d6e637
Proverbs1603 <110598358+Proverbs1603@users.noreply.github.com>
2024-10-12 09:06:34 +0900

    mixin을 활용하여 개선한 CRUD

--- polls_api/urls.py
+++ polls_api/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('question/', QuestionList.as_view(), name='question-list'),
-    path('question/<int:id>/', QuestionDetail.as_view(), name='question-detail')
+    path('question/<int:pk>/', QuestionDetail.as_view(), name='question-detail')
+    #GenericAPIView에서 아이디를 pk로 가져오기 때문에 id -> pk로 바꿔줌
 ]
--- polls_api/views.py
+++ polls_api/views.py
@@ -2,44 +2,34 @@ from rest_framework.decorators import api_view
 from polls.models import Question
 from polls_api.serializers import QuestionSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, mixins, generics
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView 
 
-class QuestionList(APIView):
-    def get(self, request):
-        questions = Question.objects.all()
-        serializer  = QuestionSerializer(questions, many = True)
-        return Response(serializer.data)    
+#ListModelMixin 은 list를 get, CreateModelMixin 은 create
+class QuestionList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Question.objects.all()    #GenericAPIView 에서 기능 제공
+    serializer_class = QuestionSerializer  #GenericAPIView 에서 기능 제공
 
-    def post(self, request):
-        serializer = QuestionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)    
 
-
-class QuestionDetail(APIView):
-    def get(self, request, id):
-        question = get_object_or_404(Question, pk=id)
-        serializer = QuestionSerializer(question)
-        return Response(serializer.data)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)  #검증 후 생성 로직이 다 구현되어있음.
+    
+#RetrieveModelMixin 은 1개를 get
+class QuestionDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
         
-    def put(self, request, id):
-        question = get_object_or_404(Question, pk=id)
-        serializer = QuestionSerializer(question, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
         
-    def delete(self, request, id):
-        question = get_object_or_404(Question, pk=id)
-        question.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
         
 
 
