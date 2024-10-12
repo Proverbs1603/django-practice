f95ed9813c4f8aa73b6901796a8e4f5950080aad
Proverbs1603 <110598358+Proverbs1603@users.noreply.github.com>
2024-10-12 14:45:15 +0900

    Form을 사용하여 user생성하기

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import SignupView
 
 app_name = 'polls'
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('/<int:question_id>/', views.detail, name='detail'),
     path('/<int:question_id>/vote/', views.vote, name='vote'),
     path('/<int:question_id>/result', views.result, name='result'),
+    path('/singup/', SignupView.as_view()),
 ]
--- polls_api/serializers.py
+++ polls_api/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from polls.models import Question
+from django.contrib.auth.models import User
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'question_text', 'pub_date']
     
+class UserSerializer(serializers.ModelSerializer):
+    #question은 user 모델에 있는 필드가 아니기 때문에 아래와 별도로 설정을 해주어야함.
+    questions = serializers.PrimaryKeyRelatedField(many=True, queryset=Question.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'questions', 'email']
+
     '''
     serializers.Serializer 가 아니라 serializers.ModelSerializer 를 상속하면 아래 코드를 작성하지 않아도 됨.
 
