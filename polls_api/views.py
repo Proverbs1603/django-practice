c8bc04c1d208935bab47cf69122d0cb6a369d66a
Proverbs1603 <110598358+Proverbs1603@users.noreply.github.com>
2024-10-12 09:20:46 +0900

    generics에서 구현된 class view를 상속받아 CRUD를 개선

--- polls_api/views.py
+++ polls_api/views.py
@@ -1,35 +1,15 @@
-from rest_framework.decorators import api_view
 from polls.models import Question
 from polls_api.serializers import QuestionSerializer
-from rest_framework.response import Response
-from rest_framework import status, mixins, generics
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView 
+from rest_framework import generics
 
-#ListModelMixin 은 list를 get, CreateModelMixin 은 create
-class QuestionList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#ListCreateAPIView는 mixin의 list와 create를 상속받음.
+class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()    #GenericAPIView 에서 기능 제공
     serializer_class = QuestionSerializer  #GenericAPIView 에서 기능 제공
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)    
+    #get,과 post가 ListCreateAPIView에 구현되어있음.
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)  #검증 후 생성 로직이 다 구현되어있음.
-    
 #RetrieveModelMixin 은 1개를 get
-class QuestionDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-        
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-        
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-        
-
-
